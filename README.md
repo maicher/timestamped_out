01e4bda8fdb690da597591e2fc89e4945a73829c
kjm <krzysztof.maicher@gmail.com>
2016-06-24 08:31:16 +0200

    Change in way of capturing the output

--- README.md
+++ README.md
@@ -20,18 +20,35 @@ Or install it yourself as:
 
 ## Usage
 
+Prints STDOUT as INFO:
+
 ```ruby
     TimestampedOut.new.call do
       print "\nTest"
     end
-    2016-05-29 14:34:18 UTC 24342 TID-ovyq9cdtw INFO: Test => #<TimestampedOut:0x007fb6ca83e4a0>
+    2016-06-23 17:08:10 +0200 95434 TID-out0sge0o INFO: Test
 ```
+
+Prints STDERR (including backtrace) as ERROR:
 
 ```ruby
     TimestampedOut.new.call do
       1 / 0
     end
-    2016-05-29 14:34:56 UTC 24342 TID-ovyq9cdtw ERROR: divided by 0
+    2016-06-23 17:20:42 +0200 95986 TID-ow6a19fy4 ERROR: divided by 0
+    ...backtrace...
+```
+
+Works also with embedded shell commands:
+
+```ruby
+    TimestampedOut.new.call do
+      system 'echo "abc"'
+      result = `echo "2"`
+      p result.to_i * 3
+    end
+    2016-06-23 17:23:07 +0200 95986 TID-ow6a2el10 INFO: abc
+    2016-06-23 17:23:07 +0200 95986 TID-ow6a2el10 INFO: 6
 ```
 
 ## Development
@@ -44,8 +61,6 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 
 Bug reports and pull requests are welcome on GitHub at https://github.com/maicher/timestamped_out. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
 
-
 ## License
 
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
-
--- lib/timestamped_out.rb
+++ lib/timestamped_out.rb
@@ -1,45 +1,49 @@
 require 'timestamped_out/version'
 
 class TimestampedOut
-  class TimestampedStringIO < StringIO
-    def initialize(stdout, level)
-      @stdout = stdout
-      @level = level
-    end
+  DEFAULT_FORMATTER = lambda { |severity, datetime, progname, message| "#{datetime} #{Process.pid} TID-#{Thread.current.object_id.to_s(36)}#{progname} #{severity}: #{message}\n" }
 
-    def write(str)
-      timestamped_str =
-        if str.length == 1 && str == "\n"
-          ''
-        elsif str[0] == "\n"
-          prepend_timestamp(str[1..-1])
-        elsif str[-1] == "\n"
-          prepend_timestamp(str[0..-2])
-        else
-          prepend_timestamp(str)
-        end
+  attr_accessor :formatter
 
-      @stdout.write(timestamped_str)
-    end
-
-    private
-
-    def prepend_timestamp(str)
-      "\n#{Time.now} #{Process.pid} TID-#{Thread.current.object_id.to_s(36)} #{@level}: #{str}"
-    end
+  def initialize(formatter = DEFAULT_FORMATTER)
+    self.formatter = formatter
   end
 
   def call
-    buffered_stdout = $stdout
-    buffered_stderr = $stderr
-    $stdout = TimestampedStringIO.new(buffered_stdout, 'INFO')
-    $stderr = TimestampedStringIO.new(buffered_stderr, 'ERROR')
+    old_stdout = STDOUT.clone
+    pipe_r, pipe_w = IO.pipe
+    pipe_r.sync = true
+    output = ''
+    reader = Thread.new do
+      begin
+        loop do
+          output << formatter.call('INFO', Time.now, '', pipe_r.readpartial(1024))
+        end
+      rescue EOFError
+      end
+    end
+    STDOUT.reopen(pipe_w)
     yield
     self
-  rescue Exception => e
-    $stderr.write "\n#{e}\n#{e.backtrace.join("\n")}"
+  rescue StandardError => e
+    STDERR.write formatter.call('ERROR', Time.now, '', "#{e}\n#{e.backtrace.join("\n")}")
   ensure
-    $stdout = buffered_stdout
-    $stderr = buffered_stderr
+    STDOUT.reopen(old_stdout)
+    pipe_w.close
+    reader.join
+    STDOUT.write(output)
+    return self
+  end
+
+  def with_timestamp(str, level)
+    if str.length == 1 && str == "\n"
+      ''
+    elsif str[0] == "\n"
+      prepend_timestamp(str[1..-1], level)
+    elsif str[-1] == "\n"
+      prepend_timestamp(str[0..-2], level)
+    else
+      prepend_timestamp(str, level)
+    end
   end
 end
